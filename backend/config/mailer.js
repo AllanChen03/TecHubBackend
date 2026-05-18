c1e6145fcd4b952ac346170a632fd2f7dc73b929
AllanChen03 <allan031199@gmail.com>
2026-05-17 20:28:52 -0600

    Arreglar server.js

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,16 +1,19 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+console.log('EMAIL:', process.env.EMAIL);         // ← agrega esto
+console.log('EMAILPASSWORD:', process.env.EMAILPASSWORD ? 'existe' : 'no existe'); // ← y esto
+
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com', 
-  port: 465,              
-  secure: true,           
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAILPASSWORD
   },
   tls: {
-    rejectUnauthorized: false 
+    rejectUnauthorized: false
   }
 });
 
